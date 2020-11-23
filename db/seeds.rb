aee724a21fd620cfbf71c283a08a75d28ee6e5a9
Joshua Parco <jacparco@gmail.com>
2020-11-23 16:00:36 -0700

    added controller for lists

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,5 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+#
+User.create(first_name:'Bob', last_name:'Smith' email:'bobsmith@gmail.com')
